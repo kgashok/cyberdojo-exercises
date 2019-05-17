628e4a02815c7d552170d7d022910cbc8ad2cc11
Ashok Bakthavathsalam <lifebalance@gmail.com>
2019-05-17 18:05:01 +0000

    moving pangram2

--- pangram/pangram2/pangram.py
+++ pangram/pangram2/pangram.py
@@ -1,20 +1,19 @@
 from orderedset import OrderedSet
 from string import ascii_lowercase as letters
 
+
 class Checker:
 
     def test(self, text):
 
-        alpha = OrderedSet(c for c in text.lower() if c.isalpha() )
+        alpha = OrderedSet(c for c in text.lower() if c.isalpha())
 
         out = "".join(alpha)
-        print (out)
+        print(out)
 
         return len(out) == 26 or self.test_partial(out)
 
-
-    def test_partial (self, pt):
+    def test_partial(self, pt):
 
         return letters.find(pt) >= 0 \
                or letters.find(pt[::-1]) >= 0
-
