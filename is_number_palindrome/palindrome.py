1217a45cbe847dbdb658ce8c09c19f2a734b47ac
Ashok Bakthavathsalam <lifebalance@gmail.com>
2020-07-25 18:58:54 +0000

    added is_palindrome

---
+++ is_number_palindrome/palindrome.py
@@ -0,0 +1,49 @@
+import math
+
+
+# solution requires two traversals
+def is_number_palindrome(number):
+    digits = []
+    number = abs(number)
+    while number:
+        digits.append(number % 10)
+        number = number // 10
+    print(digits)
+    for i in range(len(digits) // 2):
+        if digits[i] is not digits[-i - 1]:
+            return False
+    return True
+
+# reverse the digits and check if same
+def is_number_palindrome(number):
+    result, n_remaining = 0, abs(number)
+    
+    while n_remaining:
+        result = 10 * result + n_remaining % 10
+        n_remaining //= 10
+    
+    if number < 0:
+        return result == -number
+    return result == number
+
+# Use MSD mask to reverse number
+def is_number_palindrome(number):
+    
+    number = abs(number)
+    
+    try:
+        number_of_digits = math.floor(math.log10(number) + 1)
+    except ValueError:
+        # to handle number zero
+        number_of_digits = 1
+        
+    msd_mask = 10 ** (number_of_digits - 1)
+    
+    while number:
+        if number // msd_mask != number % 10:
+            return False
+        number %= msd_mask
+        number //= 10
+        msd_mask //= 100
+    
+    return True
---
+++ is_number_palindrome/test_palindrome.py
@@ -0,0 +1,23 @@
+from palindrome import is_number_palindrome
+
+def test_simple_palindrome_number():
+    assert is_number_palindrome(121) is True
+
+def test_palindrome_number_with_all_same_digits():
+    assert is_number_palindrome(888) is True
+    
+def test_non_palindrome_number_with_more_than_3_digits():
+    assert is_number_palindrome(1321) is False
+    
+def test_palindrome_number_which_is_really_big():
+    assert is_number_palindrome(12345654321) is True
+
+def test_negative_palindrome_numbers():
+    assert is_number_palindrome(-232) is True
+    assert is_number_palindrome(-12321) is True
+    
+def test_negative_non_palindrome_numbers():
+    assert is_number_palindrome(-1112) is False
+
+def test_for_zero():
+    assert is_number_palindrome(0) is True
