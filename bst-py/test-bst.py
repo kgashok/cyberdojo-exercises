ccb20b1ba53172f650a3977ad0bbd7865b545433
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2020-09-19 09:11:57 +0000

    assert validity

--- bst-py/test-bst.py
+++ bst-py/test-bst.py
@@ -7,7 +7,7 @@ def test_with_array_to_balanced_bst():
     ctree = array_to_balanced_bst(nlist)
     
     assert preorder(ctree, []) == [4, 2, 1, 3, 6, 5, 7]
-    
+    assert is_valid_bst(ctree)
     
 def test_with_a_large_array(): 
     nlist = list(range(10))
