36520faada562b04e32ba9888c2dc928549f444c
kgashok <kgashok@users.noreply.github.com>
2020-10-05 17:17:06 +0000

    Adjusted files for PEP-8 compliance

--- fixed/fixedp.py
+++ fixed/fixedp.py
@@ -8,10 +8,11 @@ def has_fixed(alst):
     for index, val in enumerate(alst):
         if val > maxindex:
             break
-        if index == val: 
+        if index == val:
             return index
-    
+
     return False
+
 
 def bin_search(alst, op):
     """
@@ -22,28 +23,27 @@ def bin_search(alst, op):
     """
     high = len(alst)-1
     low = 0
-    
-    while low <= high: 
-        mid = low + (high - low)//2 
+
+    while low <= high:
+        mid = low + (high - low)//2
         print(op, ":", low, high, mid, alst[mid])
-        
+
         if alst[mid] == mid and \
-            (mid-1 != -1 and alst[mid-1] != mid-1 or \
+            (mid-1 != -1 and alst[mid-1] != mid-1 or
              mid+1 != len(alst) and alst[mid+1] != mid+1):
             return mid
         if op == 'gt':
-            if alst[mid] > mid: 
+            if alst[mid] > mid:
                 high = mid - 1
-            else: 
+            else:
                 low = mid + 1
         else:
-            if alst[mid] > mid: 
+            if alst[mid] > mid:
                 low = mid + 1
             else:
                 high = mid - 1
     return False
 
-    
 
 def has_fixed(alst):
     """
@@ -51,7 +51,7 @@ def has_fixed(alst):
     :param alst: 
 
     """
-    res = bin_search(alst, "lt") 
-    if not res: 
+    res = bin_search(alst, "lt")
+    if not res:
         return bin_search(alst, "gt")
-    return res 
+    return res
