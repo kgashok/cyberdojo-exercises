c609dd519dd870b2470b6cf8b0ba64bbb2b085fc
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2020-10-05 17:16:16 +0000

    added fixed point code

---
+++ fixed/fixedp.py
@@ -0,0 +1,57 @@
+def has_fixed(alst):
+    """
+
+    :param alst: 
+
+    """
+    maxindex = len(alst) - 1
+    for index, val in enumerate(alst):
+        if val > maxindex:
+            break
+        if index == val: 
+            return index
+    
+    return False
+
+def bin_search(alst, op):
+    """
+
+    :param alst: 
+    :param op: 
+
+    """
+    high = len(alst)-1
+    low = 0
+    
+    while low <= high: 
+        mid = low + (high - low)//2 
+        print(op, ":", low, high, mid, alst[mid])
+        
+        if alst[mid] == mid and \
+            (mid-1 != -1 and alst[mid-1] != mid-1 or \
+             mid+1 != len(alst) and alst[mid+1] != mid+1):
+            return mid
+        if op == 'gt':
+            if alst[mid] > mid: 
+                high = mid - 1
+            else: 
+                low = mid + 1
+        else:
+            if alst[mid] > mid: 
+                low = mid + 1
+            else:
+                high = mid - 1
+    return False
+
+    
+
+def has_fixed(alst):
+    """
+
+    :param alst: 
+
+    """
+    res = bin_search(alst, "lt") 
+    if not res: 
+        return bin_search(alst, "gt")
+    return res 
---
+++ fixed/test_fixed.py
@@ -0,0 +1,52 @@
+from fixedp import has_fixed
+import pytest
+
+def test_for_fixed_point():
+    """ """
+    assert has_fixed([-6, 0, 2, 4]) == 2
+
+
+#@pytest.mark.skip
+def test_for_absence_of_fixed_point():
+    """ """
+    assert has_fixed([1, 5, 7, 8]) == False
+
+def test_for_boundary_cases():
+    """ """
+    assert has_fixed([1, 1, 4, 5]) == 1
+
+#@pytest.mark.skip
+def test_for_efficiency_of_approach():
+    """ """
+    
+    million = range(1, 1_000_000)    
+    assert has_fixed(million) == False
+    
+#@pytest.mark.skip
+def test_for_million_plus_edge_case():
+    """ """
+    
+    million = range(1, 1_000_000)
+    million_plus = list(million) + [999_999]
+    
+    zipl = list(enumerate(million_plus))
+    print(zipl[-2:])
+    assert has_fixed(million_plus) == 999_999
+
+@pytest.mark.skip
+def test_for_ten_million_plus_edge_case():
+    """ """
+    million10 = list(range(1, 16_000_000))    
+    million10_plus = million10 + [15_999_999]
+    
+    #zipl = list(enumerate(million10_plus))
+    #print(zipl[-2:])
+    assert has_fixed(million10_plus) == 15_999_999
+    
+
+def test_for_ten_million_plus_edge_case():
+    """ """
+    million10 = list(range(1, 17_000_000))    
+    m2 = [0] + million10
+    assert has_fixed(m2) == 0 
+
