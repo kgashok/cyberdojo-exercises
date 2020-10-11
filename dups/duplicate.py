cf633ac038dbdfed018e266a8560ad4ba25b5905
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2020-10-11 11:58:53 +0000

    some comments

--- dups/duplicate.py
+++ dups/duplicate.py
@@ -2,57 +2,64 @@ from collections import OrderedDict
 
 
 def remove_duplicates(alst):
+    # from stackoverflow 
     return [
         item
         for pos, item in enumerate(alst)
         if alst.index(item) == pos
     ]
 
-    #adict = dict()
+    # suggested by BA 
+    # overhead of creating a dictionary and then converting
+    # to a list
+    # adict = dict()
     # return list(adict.fromkeys(alst))
 
+    # suggested by Tarun 
+    # will not maintain order
+    # costly because of sorting
     # return sorted(list(set(alst)))
 
+    # the normal iterative code
+    # by Dayanand, improved by Sagar
     '''
     nlst = []
     for elem in alst: 
-        if elem not in nlst: 
+        if elem not in nlst:
             nlst.append(elem)
-        
     return nlst 
     '''
 
-    #nlst = [i for j, i in enumerate(alst) if i not in alst[:j]]
+    # suggested by Sagar
+    # nlst = [i for j, i in enumerate(alst) if i not in alst[:j]]
     '''
     nlst = [
-        elem for idx, elem in enumerate(alst) 
-        if elem not in alst[:idx]
+        elem
+        for pos, elem in enumerate(alst) 
+        if elem not in alst[:pos]
     ]
-    return nlst 
+    return nlst
     '''
 
-    #nlst = list(OrderedDict.fromkeys(alst))
+    # nlst = list(OrderedDict.fromkeys(alst))
     # return nlst
 
-    #alst[:] = OrderedDict.fromkeys(alst)
-    # return alst
-
+    # in-place removal of duplicates by Suveksha
     '''
     alst.sort()
-    for i in range(len(alst)-1): 
-        count = 0 
-        for j in range(i+1, len(alst)-1): 
+    for i in range(len(alst)-1):
+        count = 0
+        for j in range(i+1, len(alst)-1):
             if alst[i] == alst[j]: 
                 count += 1
-        if count != 0: 
-            for j in range(count): 
-                for k in range(i, len(alst)-1): 
+        if count != 0:
+            for j in range(count):
+                for k in range(i, len(alst)-1):
                     alst[k] = alst[k+1]
                 if j == 0: 
                     del alst[len(alst)-1:]
                 else: 
                     del alst[len(alst)-j:]
-        i += count 
-        
-    return alst 
+        i += count
+    return alst
     '''
