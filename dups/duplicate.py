507dc2157f95ad024401988996bac28a0122f307
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2020-10-11 11:41:42 +0000

    added duplicate code

---
+++ dups/duplicate.py
@@ -0,0 +1,55 @@
+from collections import OrderedDict
+def remove_duplicates(alst):
+    #return sorted(list(set(alst)))
+    
+    '''
+    nlst = []
+    for elem in alst: 
+        if elem not in nlst: 
+            nlst.append(elem)
+        
+    return nlst 
+    '''
+    
+    #nlst = [i for j, i in enumerate(alst) if i not in alst[:j]]
+    '''
+    nlst = [
+        elem for idx, elem in enumerate(alst) 
+        if elem not in alst[:idx]
+    ]
+    return nlst 
+    '''
+    
+    #nlst = list(OrderedDict.fromkeys(alst))
+    #return nlst
+
+    #alst[:] = OrderedDict.fromkeys(alst)
+    #return alst
+    adict = dict()
+    return list(adict.fromkeys(alst))
+    
+
+    '''
+    alst.sort()
+    for i in range(len(alst)-1): 
+        count = 0 
+        for j in range(i+1, len(alst)-1): 
+            if alst[i] == alst[j]: 
+                count += 1
+        if count != 0: 
+            for j in range(count): 
+                for k in range(i, len(alst)-1): 
+                    alst[k] = alst[k+1]
+                if j == 0: 
+                    del alst[len(alst)-1:]
+                else: 
+                    del alst[len(alst)-j:]
+        i += count 
+        
+    return alst 
+    '''                
+                    
+                    
+                
+                
+                
---
+++ dups/test_duplicate.py
@@ -0,0 +1,34 @@
+from duplicate import remove_duplicates
+
+def test_list_with_no_elements():
+    arr = []
+    
+    assert remove_duplicates(arr) == [] 
+    
+
+def test_list_with_no_duplicates(): 
+    arr = [1, 2, 3]
+    
+    assert remove_duplicates(arr) == [1, 2, 3]
+    
+def test_list_with_one_duplicate():     
+    arr = [1, 1, 2, 3]
+    assert remove_duplicates(arr) == [1, 2, 3]
+    
+    arr = [100, 100, 4, 3, 2]
+    #arr.sort()
+    #assert remove_duplicates(arr) == arr
+    
+    assert remove_duplicates(arr) == [100, 4, 3, 2]
+    
+from random import shuffle
+def test_list_with_lot_of_duplicates(): 
+    
+    arr = list(range(10))
+    dup_arr = arr * 2 
+    li = remove_duplicates(dup_arr)
+    assert li == arr
+    
+    shuffle(dup_arr)
+    assert all([x in li for x in dup_arr])
+
