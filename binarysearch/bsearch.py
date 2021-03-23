1414ddfd8d46e0ef75eb7bac82ee78efc71d9a7f
kgashok <kgashok@users.noreply.github.com>
2021-03-23 08:18:34 +0000

    Adjusted files for PEP-8 compliance

--- binarysearch/bsearch.py
+++ binarysearch/bsearch.py
@@ -1,24 +1,23 @@
-def binary_search(alist, token): 
-    # initialize markers at the edges of the list 
+def binary_search(alist, token):
+    # initialize markers at the edges of the list
     left = 0
     right = len(alist) - 1
-    
-    while left <= right: 
+
+    while left <= right:
         mid = (left + right) // 2
         midvalue = alist[mid]
-        
-        #print(f'left:{left} right:{right} mid:{mid} midval:{midval}')
+
+        # print(f'left:{left} right:{right} mid:{mid} midval:{midval}')
         if midvalue == token:
             print("Token", token, "Found at", mid)
             return mid
-        
+
         if token > midvalue:
             # if token is 7 and midval is 5...
             # shift left marker to the right of mid
             left = mid + 1
-        else: 
+        else:
             right = mid - 1
-    
-    print("Token", token, "Not found!")
-    return -1 
 
+    print("Token", token, "Not found!")
+    return -1
--- binarysearch/test_bsearch.py
+++ binarysearch/test_bsearch.py
@@ -1,34 +1,40 @@
-from bsearch import binary_search 
-import pytest 
+from bsearch import binary_search
+import pytest
 
 pytestmark = pytest.mark.random_order(disabled=True)
 
-def test_finding_5_at_index_2_in_list_containing_1_3_5_7_9():
-    alist = [1, 3, 5, 7, 9]   
-    assert binary_search(alist, 5) == 2
-    assert binary_search([1, 3, 5], 3) == 1 
 
-    
+def test_finding_5_at_index_2_in_list_containing_1_3_5_7_9():
+    alist = [1, 3, 5, 7, 9]
+    assert binary_search(alist, 5) == 2
+    assert binary_search([1, 3, 5], 3) == 1
+
+
 def test_finding_7_at_index_3_in_list_containing__1_3_5_7_9():
-    alist = [1, 3, 5, 7, 9]    
+    alist = [1, 3, 5, 7, 9]
     assert binary_search(alist, 7) == 3
-    
+
+
 def test_not_finding_2_at_index_2_in_list_containing__1_3_5_7_9():
-    alist = [1, 3, 5, 7, 9]    
+    alist = [1, 3, 5, 7, 9]
     assert binary_search(alist, 2) == -1
 
-def test_not_finding_10__in_the_list_containing_odd_numbers():
-    alist = [1, 3, 5, 7, 9] 
-    assert binary_search(alist, 10) == -1 
 
-#@pytest.mark.skip
+def test_not_finding_10__in_the_list_containing_odd_numbers():
+    alist = [1, 3, 5, 7, 9]
+    assert binary_search(alist, 10) == -1
+
+# @pytest.mark.skip
+
+
 def test_finding_1_at_index_0_in_list_containing__1_3_5_7_9():
     alist = [1, 3, 5, 7, 9]
     assert binary_search(alist, 1) == 0
 
-#@pytest.mark.skip
+# @pytest.mark.skip
+
+
 def test_finding_3_at_index_1_in_list_containing__1_3_5_7_9():
     alist = [1, 3, 5, 7, 9]
-    
+
     assert binary_search(alist, 3) == 1
-    
