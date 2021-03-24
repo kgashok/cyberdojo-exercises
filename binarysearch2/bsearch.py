6f327c89cc336e423dc449d8e50aa6146cad1816
kgashok <kgashok@users.noreply.github.com>
2021-03-24 12:10:39 +0000

    Adjusted files for PEP-8 compliance

--- binarysearch2/bsearch.py
+++ binarysearch2/bsearch.py
@@ -1,19 +1,19 @@
 def binary_search(alist, token):
-    
+
     while alist:
         mid = len(alist) // 2
         midvalue = alist[mid]
-    
+
         print(f'\nmid:{mid} midvalue:{midvalue}')
         if token == midvalue:
             return True
-    
+
         if token < midvalue:
             # truncate alist to lower half
             alist = alist[:mid]
-            
+
         else:
             # truncate alist to upper half
             alist = alist[mid + 1:]
-      
+
     return False
