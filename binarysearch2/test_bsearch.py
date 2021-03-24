c6346d566a402877b64215ce768dce0f549124d4
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-03-24 17:42:30 +0530

    singleton asserts

--- binarysearch2/test_bsearch.py
+++ binarysearch2/test_bsearch.py
@@ -5,20 +5,20 @@ import pytest
 def test_finding_5_in_list__1_3_5_7_9():
     '''best case of finding in the middle of list'''
     alist = [1, 3, 5, 7, 9]
-    assert binary_search(alist, 5) == True
+    assert binary_search(alist, 5) is True
 
 
 @pytest.mark.skip
 def test_not_finding_4_in_list__1_3_5_7_9():
     alist = [1, 3, 5, 7, 9]
-    assert binary_search(alist, 4) == False
+    assert binary_search(alist, 4) is False
 
 
 def test_finding_3_in_list__1_3_5_7_9():
     alist = [1, 3, 5, 7, 9]
-    assert binary_search(alist, 3) == True
+    assert binary_search(alist, 3) is True
 
 
 def test_finding_7_in_list__1_3_5_7_9():
     alist = [1, 3, 5, 7, 9]
-    assert binary_search(alist, 7) == True
+    assert binary_search(alist, 7) is True
