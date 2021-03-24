5536195547a14d896e92e143f49989f12f7bc2b3
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-03-24 17:46:00 +0530

    minor edits

--- binarysearch2/bsearch.py
+++ binarysearch2/bsearch.py
@@ -7,11 +7,9 @@ def binary_search(alist, token):
         print(f'\nmid:{mid} midvalue:{midvalue}')
         if token == midvalue:
             return True
-
         if token < midvalue:
             # truncate alist to lower half
             alist = alist[:mid]
-
         else:
             # truncate alist to upper half
             alist = alist[mid + 1:]
