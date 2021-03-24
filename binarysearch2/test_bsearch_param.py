01d2647f9df23abc094a51c8ecb900d2625114c8
kgashok <kgashok@users.noreply.github.com>
2021-03-24 17:42:15 +0000

    Adjusted files for PEP-8 compliance

--- binarysearch2/test_bsearch_param.py
+++ binarysearch2/test_bsearch_param.py
@@ -1,22 +1,23 @@
 from bsearch import binary_search
 
 import pytest
-pytestmark = pytest.mark.random_order(disabled=True) 
+pytestmark = pytest.mark.random_order(disabled=True)
+
 
 @pytest.fixture
 def alist():
     return [1, 3, 5, 7, 9]
 
+
 @pytest.mark.parametrize("alist, token, expected",
-    [(alist, 5, True),
-     (alist, 3, True),
-     (alist, 7, True), 
-     (alist, 1, True), 
-     (alist, 9, True), 
-     (alist, 4, False),
-     (alist, 12, False)
-    ], indirect=["alist"]
-)
+                         [(alist, 5, True),
+                          (alist, 3, True),
+                             (alist, 7, True),
+                             (alist, 1, True),
+                             (alist, 9, True),
+                             (alist, 4, False),
+                             (alist, 12, False)
+                          ], indirect=["alist"]
+                         )
 def test_finding_token_in_sample_list(alist, token, expected):
     assert binary_search(alist, token) is expected
-
