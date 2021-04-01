2b0916fbf39fd78fbbf53b2fdb07c0e5eb07de0c
kgashok <kgashok@users.noreply.github.com>
2021-04-01 19:31:16 +0000

    Adjusted files for PEP-8 compliance

--- reversedict/reversedict.py
+++ reversedict/reversedict.py
@@ -1,11 +1,12 @@
-from bisect import insort 
+from bisect import insort
+
 
 def update(d, k, v):
     if k not in d:
         d[k] = v
     else:
         if type(d[k]) is not list:
-            d[k] = [ d[k] ]
+            d[k] = [d[k]]
         try:
             # if v is a single value
             insort(d[k], v)
@@ -14,40 +15,42 @@ def update(d, k, v):
             error_string = str(e)
             if "list" in error_string and "int" in error_string:
                 print("-->> Handling list of values...", v)
-            for elem in v: 
+            for elem in v:
                 insort(d[k], elem)
+
 
 def reverse_dict2(adict):
     res = dict()
     for key, value in adict.items():
         print(f'key:{key}, value:{value}')
         if type(value) is not list:
-            update(res, value, key)            
+            update(res, value, key)
         else:
             ldict = dict()
             for elem in value:
                 update(ldict, elem, key)
             print(f'ldict:{ldict}')
-            for k, v in ldict.items(): 
+            for k, v in ldict.items():
                 update(res, k, v)
         print(f"---> INTER:{res}")
-            
+
     return res
 
+
 def reverse_dict(adict):
-    
+
     res = dict()
     for key, value in adict.items():
-        #print(f'\nkey:{key}, value:{value}', end="-->")
-        
+        # print(f'\nkey:{key}, value:{value}', end="-->")
+
         if type(value) is list:
             ldict = dict.fromkeys(value, key)
-            #print(f'ldict:{ldict}')
-            for key, value in ldict.items(): 
+            # print(f'ldict:{ldict}')
+            for key, value in ldict.items():
                 update(res, key, value)
-            #print(f'res:{res}')
+            # print(f'res:{res}')
         else:
             update(res, value, key)
-        #print(f'res:{res}')
-    
+        # print(f'res:{res}')
+
     return res
