bd7dc814d27823e31acc6396e5e8cebb36c25479
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-04-02 01:00:10 +0530

    some more tests

--- reversedict/reversedict.py
+++ reversedict/reversedict.py
@@ -1,51 +1,53 @@
-from bisect import insort
-
+from bisect import insort 
 
 def update(d, k, v):
     if k not in d:
         d[k] = v
     else:
         if type(d[k]) is not list:
-            d[k] = [d[k]]
+            d[k] = [ d[k] ]
         try:
             # if v is a single value
             insort(d[k], v)
-        except TypeError:
-            # then it must be a list of values
-            for elem in v:
+        except TypeError as e:
+            # then v must be a list of values
+            error_string = str(e)
+            if "list" in error_string and "int" in error_string:
+                print("-->> Handling list of values...", v)
+            for elem in v: 
                 insort(d[k], elem)
-
 
 def reverse_dict2(adict):
     res = dict()
     for key, value in adict.items():
+        print(f'key:{key}, value:{value}')
         if type(value) is not list:
-            update(res, value, key)
+            update(res, value, key)            
         else:
             ldict = dict()
             for elem in value:
                 update(ldict, elem, key)
-            for k, v in ldict.items():
+            print(f'ldict:{ldict}')
+            for k, v in ldict.items(): 
                 update(res, k, v)
         print(f"---> INTER:{res}")
-
+            
     return res
 
-
 def reverse_dict(adict):
-
+    
     res = dict()
     for key, value in adict.items():
-        # print(f'\nkey:{key}, value:{value}', end="-->")
-
+        #print(f'\nkey:{key}, value:{value}', end="-->")
+        
         if type(value) is list:
             ldict = dict.fromkeys(value, key)
-            # print(f'ldict:{ldict}')
-            for key, value in ldict.items():
+            #print(f'ldict:{ldict}')
+            for key, value in ldict.items(): 
                 update(res, key, value)
-            # print(f'res:{res}')
+            #print(f'res:{res}')
         else:
             update(res, value, key)
-        # print(f'res:{res}')
-
+        #print(f'res:{res}')
+    
     return res
--- reversedict/test_reversedict.py
+++ reversedict/test_reversedict.py
@@ -1,72 +1,67 @@
 from reversedict import reverse_dict, reverse_dict2
 import pytest
 
-pytestmark = pytest.mark.random_order(disabled=True)
-
+pytestmark = pytest.mark.random_order(disabled=True) 
 
 def test_reversing_a_simple_dict():
-    adict = {1: 2}
-    expected = {2: 1}
+    adict = {1:2}
+    expected = {2:1}
 
     assert reverse_dict(adict) == expected
-
-
+    
 def test_reversing_a_dict_with_two_keys():
-    adict = {1: 2, 3: 4}
-    expected = {2: 1, 4: 3}
-    assert reverse_dict(adict) == expected
-
-
-def test_reversing_a_dict_containing_char_key_and_number_value():
+    adict = {1:2, 3:4}
+    expected = {2:1, 4:3} 
+    assert reverse_dict(adict) == expected 
+    
+def test_reversing_a_dict_containing_char_key_and_number_value(): 
     adict = {'ajitha': 9000, 'vidhya': 1000}
-    expected = {9000: 'ajitha', 1000: 'vidhya'}
-
-    assert reverse_dict(adict) == expected
-
-
-def test_reversing_a_dict_containing_duplicate_values():
-    adict = {'ajitha': 9000, 'vidhya': 9000, 'satish': 2000}
-    expected = {9000: ['ajitha', 'vidhya'], 2000: 'satish'}
-    assert reverse_dict(adict) == expected
-
+    expected = {9000:'ajitha', 1000:'vidhya'} 
+    
+    assert reverse_dict(adict) == expected 
+    
+def test_reversing_a_dict_containing_duplicate_values(): 
+    adict = {'ajitha':9000, 'vidhya':9000, 'satish': 2000}
+    expected = {9000:['ajitha', 'vidhya'], 2000:'satish'}    
+    assert reverse_dict(adict) == expected 
 
 def test_reversing_a_dict_containing_duplicates_values_not_in_order():
-    adict = {'vidhya': 9000, 'ajitha': 9000, 'satish': 2000}
-    expected = {9000: ['ajitha', 'vidhya'], 2000: 'satish'}
-    assert reverse_dict(adict) == expected
-
+    adict = {'vidhya':9000, 'ajitha':9000, 'satish': 2000}
+    expected = {9000:['ajitha', 'vidhya'], 2000:'satish'}
+    assert reverse_dict(adict) == expected 
 
 def test_reversing_dict_containing_list_as_value():
-    adict = {1: 2, 2: [3, 4, 5]}
-    expected = {2: 1, 3: 2, 4: 2, 5: 2}
+    adict = {1:2,2:[3,4,5]}
+    expected ={2:1,3:2,4:2,5:2}
     assert reverse_dict(adict) == expected
-
 
 def test_reversing_dict_containing_list_as_value_with_duplicates():
-    adict = {1: 2, 2: [2, 3, 4, 5]}
-    expected = {2: [1, 2], 3: 2, 4: 2, 5: 2}
-    assert reverse_dict(adict) == expected
+    adict = {1:2, 2:[2, 3, 4, 5]}
+    expected = {2:[1, 2], 3:2, 4:2, 5:2}
+    assert reverse_dict(adict) == expected 
 
 
 @pytest.mark.parametrize(
     "adict, expected", [
-        ({1: 2, 2: [2, 3, 4, 5, 2]},
-         {2: [1, 2], 3:2, 4:2, 5:2}),
-        ({1: 2, 2: [2, 3, 4], 4:[5, 3, 2]},
-         {2: [1, 2, 4], 3: [2, 4], 4:2, 5:4}),
+        ({1:2, 2:[2, 3, 4, 5, 2]}, 
+         {2:[1, 2], 3:2, 4:2, 5:2}), 
+        ({1:2, 2:[2, 3, 4], 4:[5, 3, 2]}, 
+         {2:[1, 2, 4], 3: [2, 4], 4:2, 5:4}),
     ]
 )
 def test_reversing_dict_containing_list_as_value_with_multiple_dups(adict, expected):
-    assert reverse_dict(adict) == expected
-
-
+    assert reverse_dict(adict) == expected  
+    
 @pytest.mark.parametrize(
     "adict, expected", [
-        ({1: 2, 2: [2, 3, 4, 5, 2]},
-         {2: [1, 2, 2], 3:2, 4:2, 5:2}),
-        ({1: 2, 2: [2, 3, 4], 4:[5, 5, 3, 2], 100:5},
-         {2: [1, 2, 4], 3: [2, 4], 4:2, 5:[4, 4, 100]}),
+        ({1:2, 2:[2, 3, 4, 5, 2]}, 
+         {2:[1, 2, 2], 3:2, 4:2, 5:2}), 
+        ({1:2, 2:[2, 3, 4], 4:[5, 5, 3, 2], 100:5}, 
+         {2:[1, 2, 4], 3: [2, 4], 4:2, 5:[4, 4, 100]}),
+        ({1:2, 2:[2, 0, 0, 0, 3], 100:2}, 
+         {2:[1, 2, 100], 0:[2, 2, 2], 3:2}),
     ]
 )
 def test_reversing_dict_containing_list_as_value_with_multiple_dups(adict, expected):
-    assert reverse_dict2(adict) == expected
+    assert reverse_dict2(adict) == expected  
+
