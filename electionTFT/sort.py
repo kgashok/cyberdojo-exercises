ac6575b9e421511170ecf354d2b11180c02d1205
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-04-07 02:01:13 +0530

    if statement required

--- electionTFT/sort.py
+++ electionTFT/sort.py
@@ -29,16 +29,22 @@ def insert_into_sorted_array(alist, key, kindex):
     j = kindex
     while j > 0 and key < alist[j-1]:
         j -= 1
+    
+    if j != kindex:
+        # print("\nBefore shift:", alist)
+        # shift right by 1 position, (kindex-j) elements
+        alist[j+1:kindex+1] = alist[j:kindex]
+        # print("After shift:", alist)
+        # insert key into the array 
+        alist[j] = key
+        # print("After insertion", alist)
+    
 
-    # print("\nBefore shift:", alist)
-    # shift right by 1 position, (kindex-j) elements
-    alist[j+1:kindex+1] = alist[j:kindex]
-    # print("After shift:", alist)
-    # insert key into the array
-    alist[j] = key
-    # print("After insertion", alist)
-
-
+def insertion_sort(alist):
+    for i in range(1, len(alist)):
+        key = alist[i]
+        insert_into_sorted_array(alist, key, i)
+    return alist
 def insertion_sort(alist):
     for i in range(1, len(alist)):
         key = alist[i]
