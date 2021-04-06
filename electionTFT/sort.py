167e7a6af178bda9de19131ea9f5e8261252af41
Ashok Bakthavathsalam <kgashok@users.noreply.github.com>
2021-04-07 02:11:13 +0530

    minor edits

--- electionTFT/sort.py
+++ electionTFT/sort.py
@@ -11,7 +11,7 @@ def insert_into_sorted_array(alist, key, index):
     while index > 0 and key < alist[index-1]:
         # alist[index] = alist[index-1]
         index -= 1
-    # alist[index] = key
+    #alist[index] = key
     print("before", alist, key, index)
     alist.insert(index, key)
     print("after", alist)
@@ -24,28 +24,20 @@ def insert_into_sorted_array(alist, key, index):
         index -= 1
     alist[index] = key
 
-
+    
 def insert_into_sorted_array(alist, key, kindex):
     j = kindex
     while j > 0 and key < alist[j-1]:
         j -= 1
-
-    if j != kindex:
-        # print("\nBefore shift:", alist)
-        # shift right by 1 position, (kindex-j) elements
-        alist[j+1:kindex+1] = alist[j:kindex]
-        # print("After shift:", alist)
-        # insert key into the array
-        alist[j] = key
-        # print("After insertion", alist)
-
-
-def insertion_sort(alist):
-    for i in range(1, len(alist)):
-        key = alist[i]
-        insert_into_sorted_array(alist, key, i)
-    return alist
-
+    
+    print("\nBefore shift:", alist, j, kindex)
+    # shift right by 1 position, (kindex-j) elements
+    alist[j+1:kindex+1] = alist[j:kindex]
+    print("After shift:", alist)
+    # insert key into the array 
+    alist[j] = key
+    # print("After insertion", alist)
+    
 
 def insertion_sort(alist):
     for i in range(1, len(alist)):
