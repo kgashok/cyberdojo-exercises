9a2baa30387cbb781f5922dc5665bd54cf04d915
Ashok B <lifebalance@gmail.com>
2021-12-27 13:29:13 +0530

    ternary use case

--- validate/validate.py
+++ validate/validate.py
@@ -16,11 +16,10 @@ def validate(number):
     total += sum(other_digits)
     print("total without check_digit", total)
 
-    correct_check = None
-    if total % 10 == 0 and check_digit:
-        correct_check = 0
-    elif (total + check_digit) % 10:
-        correct_check = 10 - (total % 10)
+    correct_check = 0 if not total % 10 and check_digit else \
+        10 - (total % 10) if (total + check_digit) % 10 else \
+        None
+        
     check_string = " " + str(correct_check)
 
     return "IN" * (correct_check is not None) \
