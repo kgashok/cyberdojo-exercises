720b2920e84c71d06127139802f159d22d9e992f
kgashok <kgashok@users.noreply.github.com>
2021-12-27 04:08:24 +0000

    Adjusted files for PEP-8 compliance

--- validate/validate.py
+++ validate/validate.py
@@ -1,31 +1,31 @@
 def validate(number):
-	digits = list(int(i) for i in str(number))
+    digits = list(int(i) for i in str(number))
 
-	digits_to_double = digits[-2::-2]
-	other_digits = digits[-3::-2]
-	check_digit = digits[-1]
-	print(digits_to_double, other_digits, check_digit)
+    digits_to_double = digits[-2::-2]
+    other_digits = digits[-3::-2]
+    check_digit = digits[-1]
+    print(digits_to_double, other_digits, check_digit)
 
-	total = 0
-	for d in digits_to_double:
-		sumdigits = sum(int(i) for i in list(str(d*2)))
-		total += sumdigits
-	
-	total += sum(other_digits)
-	print("total without check_digit", total)
+    total = 0
+    for d in digits_to_double:
+        sumdigits = sum(int(i) for i in list(str(d*2)))
+        total += sumdigits
 
-	correct_check = None 
-	if total % 10 == 0 and check_digit:
-		correct_check = 0
-	elif (total + check_digit) % 10: 
-		correct_check = 10 - (total % 10)	
-	check_string = " " + str(correct_check) 
+    total += sum(other_digits)
+    print("total without check_digit", total)
 
-	return "IN" * (correct_check is not None) \
-		+ "VALID" \
-		+ ("" if correct_check is None else check_string)
+    correct_check = None
+    if total % 10 == 0 and check_digit:
+        correct_check = 0
+    elif (total + check_digit) % 10:
+        correct_check = 10 - (total % 10)
+    check_string = " " + str(correct_check)
 
-	
+    return "IN" * (correct_check is not None) \
+        + "VALID" \
+        + ("" if correct_check is None else check_string)
+
+
 if __name__ == "__main__":
-	acctid = int(input("Account Number to validate: ")) 
-	print(validate(acctid))
+    acctid = int(input("Account Number to validate: "))
+    print(validate(acctid))
