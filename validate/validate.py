a124d2f9f950c51a325243133b88caea12db0f50
kgashok <kgashok@users.noreply.github.com>
2021-12-27 03:45:23 +0000

    Adjusted files for PEP-8 compliance

--- validate/validate.py
+++ validate/validate.py
@@ -1,19 +1,18 @@
 def validate(number):
     digits = list(str(number))[::-1]
     double = ""
-    for i in range(len(digits)):         
+    for i in range(len(digits)):
         if (i % 2 != 0):
-            double  += str(int(digits[i]) * 2)
+            double += str(int(digits[i]) * 2)
         else:
-            double += digits[i]    
-    total = sum([int(x) for x in list(double)])  
+            double += digits[i]
+    total = sum([int(x) for x in list(double)])
     print("\nnumber", number)
     print("doubled as", double, "\ntotal", total)
     if (total % 10 == 0):
         return "VALID"
     else:
-        bad_check     = number % 10
+        bad_check = number % 10
         revised_total = total - bad_check
-        good_check    = 10 - (revised_total % 10)
+        good_check = 10 - (revised_total % 10)
         return "INVALID " + str(good_check)
-        
